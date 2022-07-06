6d34c9925f35ab9b5da80c5b65d164dabdbdade0
amarjeet <jaiswaramarjeet19@cognizant.com>
2022-07-06 16:07:12 +0530

    microservice 2

---
+++ User-FlightService/src/main/java/com/userFlight/UserFlightServiceApplication.java
@@ -0,0 +1,13 @@
+package com.userFlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFlightServiceApplication.class, args);
+	}
+
+}
