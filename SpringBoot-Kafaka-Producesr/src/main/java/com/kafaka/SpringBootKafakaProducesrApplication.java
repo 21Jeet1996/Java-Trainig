a9ddd97e44a0f28d4b100abcd95355ce3b0a38ad
amarjeet <jaiswaramarjeet19@cognizant.com>
2022-07-06 16:08:11 +0530

    kafaka

---
+++ SpringBoot-Kafaka-Producesr/src/main/java/com/kafaka/SpringBootKafakaProducesrApplication.java
@@ -0,0 +1,13 @@
+package com.kafaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafakaProducesrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafakaProducesrApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Kafaka-Producesr/src/test/java/com/kafaka/SpringBootKafakaProducesrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafakaProducesrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
