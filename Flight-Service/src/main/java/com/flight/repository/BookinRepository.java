1fba9bf65615a84778ffa8c43bc26572654d5b0d
amarjeet <jaiswaramarjeet19@cognizant.com>
2022-07-18 15:53:52 +0530

    delete

--- Flight-Service/src/main/java/com/flight/repository/BookinRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flight.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.flight.entity.BookingEntity;
-
-public interface BookinRepository extends JpaRepository<BookingEntity,Long>{
-
-}
--- SpringBootSecurityJWT/src/main/java/com/security/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.security.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.security.entity.CoustomerEntity;
-
-public interface CustomerRepository extends JpaRepository<CoustomerEntity,Integer>{
-
-}
